90da82b7fbae5ee3c2cdb51346c8847b101db618
Matheus Rodrigues <mathteusrrt1@gmail.com>
2026-05-05 14:11:53 -0300

    ajuste no .yml

--- telemetria/src/main/java/visioncontrol/mensageria/telemetria/controller/TelemetriaController.java
+++ telemetria/src/main/java/visioncontrol/mensageria/telemetria/controller/TelemetriaController.java
@@ -1,15 +1,12 @@
 package visioncontrol.mensageria.telemetria.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import visioncontrol.mensageria.telemetria.business.consumer.TelemetriaConsumer;
-import visioncontrol.mensageria.telemetria.infrastructure.entity.TelemetriaEntity;
 
 import java.util.Map;
 
