7025fd4ed74b77b4bf5ca1872c849544760837b3
Matheus Rodrigues <mathteusrrt1@gmail.com>
2026-05-07 14:12:44 -0300

    mudança no tipo do ID para facilitar migração de banco de dados

--- telemetria/src/main/java/visioncontrol/mensageria/telemetria/infrastructure/entity/TelemetriaEntity.java
+++ telemetria/src/main/java/visioncontrol/mensageria/telemetria/infrastructure/entity/TelemetriaEntity.java
@@ -17,8 +17,8 @@ import java.time.LocalDateTime;
 public class TelemetriaEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
 
     @Column(columnDefinition = "TEXT")
     private String payload;
