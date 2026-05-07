f9c9e8dea67c3ae8d1d237ae94c9426bbe5d491b
Matheus Rodrigues <mathteusrrt1@gmail.com>
2026-05-07 10:21:16 -0300

    correcao na controller

--- telemetria/src/main/java/visioncontrol/mensageria/telemetria/controller/TelemetriaController.java
+++ telemetria/src/main/java/visioncontrol/mensageria/telemetria/controller/TelemetriaController.java
@@ -8,10 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/webhook")
 @RequiredArgsConstructor
 @Slf4j
