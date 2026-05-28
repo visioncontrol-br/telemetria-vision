e3171662195e9a1573180ae5469bbad91e1f5f4c
Matheus Rodrigues <mathteusrrt1@gmail.com>
2026-05-28 11:07:40 -0300

    deletado posicoesClaudeRepository

--- telemetria/src/main/java/visioncontrol/mensageria/telemetria/infrastructure/repository/PosicoesClaudRepository.java
+++
@@ -1,8 +0,0 @@
-package visioncontrol.mensageria.telemetria.infrastructure.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface PosicoesClaudRepository extends JpaRepository<PosicoesClaudEntity, UUID> {
-}
