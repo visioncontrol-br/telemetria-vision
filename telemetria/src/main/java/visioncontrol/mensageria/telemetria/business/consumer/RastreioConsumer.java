601bc667c3ae61999ae681aceff29fcf21c6aee3
Matheus Rodrigues <mathteusrrt1@gmail.com>
2026-06-11 11:50:30 -0300

    ajuste no consumer

--- telemetria/src/main/java/visioncontrol/mensageria/telemetria/business/consumer/RastreioConsumer.java
+++ telemetria/src/main/java/visioncontrol/mensageria/telemetria/business/consumer/RastreioConsumer.java
@@ -1,11 +1,9 @@
 package visioncontrol.mensageria.telemetria.business.consumer;
 
 import com.rabbitmq.client.Channel;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -14,10 +12,7 @@ import java.util.List;
 
 @Component
 @Slf4j
-@RequiredArgsConstructor
 public class RastreioConsumer {
-
-    private final JdbcTemplate jdbcTemplate;
 
     @RabbitListener(
             queues = "${app.rabbitmq.queue}",
@@ -28,24 +23,19 @@ public class RastreioConsumer {
         long lastDeliveryTag = messages.get(messages.size() - 1).getMessageProperties().getDeliveryTag();
 
         try {
-            // Dispara o lote de Strings brutas para a função do Postgres
-            jdbcTemplate.batchUpdate(
-                    "SELECT public.rotear_payload_veiculo(?::jsonb)",
-                    messages,
-                    messages.size(),
-                    (ps, message) -> {
-                        String jsonBruto = new String(message.getBody(), StandardCharsets.UTF_8);
-                        ps.setString(1, jsonBruto);
-                    }
-            );
+            log.info("==================================================");
+            log.info("LOTE RECEBIDO DO RABBITMQ! Tamanho do lote: {}", messages.size());
 
-            // Confirma o lote inteiro no RabbitMQ de uma vez só
+            // Imprime apenas a primeira mensagem do lote para nao poluir muito o log
+            String primeiroJson = new String(messages.get(0).getBody(), StandardCharsets.UTF_8);
+            log.info("Conteudo da 1a Mensagem: {}", primeiroJson);
+            log.info("==================================================");
+
+            // Confirma para o RabbitMQ que o lote inteiro foi "processado" (Aba 'Unacked' vai zerar)
             channel.basicAck(lastDeliveryTag, true);
-            log.info("[SUCESSO] Lote de {} mensagens processado pelo PostgreSQL.", messages.size());
 
         } catch (Exception e) {
-            log.error("Erro no lote de mensagens. Enviando bloco para a DLQ.", e);
-            // Em caso de qualquer erro de banco, o lote inteiro vai para a DLQ para auditoria
+            log.error("Erro ao simular o processamento do lote.", e);
             channel.basicNack(lastDeliveryTag, true, false);
         }
     }
